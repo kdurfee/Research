a46a44a6cd7ca5f2b6fb48504c2d7a22c0d8ab50
kdurfee <durfguy@gmail.com>
2018-06-04 11:21:54 -0700

    arch sim for simple example forward pass matching golden

---
+++ arch/ArchLib.py
@@ -0,0 +1,182 @@
+import numpy as np
+
+def Convolve(act,weights):
+    #input:
+    #activation of shape C,H,W
+    #weight of shape K,C,H,W
+    #we assume stride and no padding here
+
+    #check if case where C is just one, and this is 2D
+    if act.ndim==2 and weights.ndim==2:
+        H,W=act.shape
+        HH,WW=weights.shape
+        OH = int((H-HH)+1)
+        OW = int((W-WW)+1)
+        out = np.zeros((OH,OW))
+        for j in range(OH):
+            for k in range(OW):
+                input = act[j:j+HH,k:k+WW]
+                filter=weights
+                result = np.sum(input*filter,axis=(0,1))
+                out[j,k]=result
+    #-------end of 2 dim ------------------
+    else: #------ 3 dim ----------
+        C,H,W=act.shape
+        K,C,HH,WW=weights.shape
+
+        #for now, assume no padding and stride of one
+        #(H-HH+2*pad)/stride+1
+        OH = int((H-HH)+1)
+        OW = int((W-WW)+1)
+        out = np.zeros((K,OH,OW))
+        
+        for i in range(K):
+            for j in range(OH):
+                for k in range(OW):
+                    input = act[:,j:j+HH,k:k+WW]
+                    filter = weights[i]
+                    result = np.sum(input*filter,axis=(0,1,2))
+                    out[i:i+1,j,k]=result
+    #--------end of 3 dim
+    return out
+    
+class PE:
+    def __init__(self,x,y):
+        self.weights = dict() #dictionary to store layer weights with a kernel lookup
+        self.id=(x,y) #store location in PE grid
+        self.inAct=dict() #variable for input activations
+        self.outAct=dict() #variablef or output activaitons
+        self.inBuf=None
+        self.outBuf=None
+
+    def SetInAct(self,layer,act):
+        self.inAct[layer]=act
+
+    def LoadInBuf(self,data):
+        self.inBuf=data
+    def FlushInBuf(self):
+        self.inBuf=None
+    def FlushOutBuf(self):
+        self.outBuf=None
+        
+    def ForwardConv(self,kernel,channel):
+        conv = Convolve(self.inBuf,self.weights[kernel][channel,:,:])
+        if self.outBuf==None:
+            H,W=conv.shape
+            self.outBuf=np.zeros((H,W))
+            
+        self.outBuf=np.add(self.outBuf,conv)
+
+    def PrintInAct(self,layer):
+        print("PE ID is {x},{y}".format(x=self.id[0],y=self.id[1]))
+        print("Input Activations for layer {x}".format(x=layer))
+        print (self.inAct[layer])
+
+    def PrintInBuf(self):
+        print("PE ID is {x},{y}".format(x=self.id[0],y=self.id[1]))
+        print("Input Buffer")
+        print (self.inBuf)        
+
+    def PrintWeights(self,kernel):
+        print("PE ID is {x},{y}".format(x=self.id[0],y=self.id[1]))
+        print("Weights for kernel {k}".format(k=kernel))
+        print (self.weights[kernel])
+
+    def PrintOutAct(self,layer):
+        print("PE ID is {x},{y}".format(x=self.id[0],y=self.id[1]))
+        print("Output Activations for layer {x}".format(x=layer))
+        print(self.outAct[layer])
+
+    def PrintOutBuf(self):
+        print("PE ID is {x},{y}".format(x=self.id[0],y=self.id[1]))
+        print("Output Buffer")
+        print (self.outBuf)            
+        
+class Core:
+    def __init__(self,size=4):
+        self.PEGrid=dict()
+        self.size=size
+        for i in range(0,size):
+            for j in range (0,size):
+                self.PEGrid[i,j]=PE(i,j)
+
+    def PrintPEGrid(self):
+        for i in range(0,self.size):
+            print ("")
+            for j in range(0,self.size):
+                print (self.PEGrid[i,j].id,end="")
+        print("")
+
+    def RowBroadcast(self,row,col,layer):
+        #in the specified row, take the activation information stored in the given column
+        #and broadcast it to the input buffer of all PEs in that row (including broadcaster)
+        for c in range (0,self.size):            
+            self.PEGrid[row,c].LoadInBuf(self.PEGrid[row,col].inAct[layer])
+            #TODO currently one channel only
+            
+    def ColReduce(self,row,col):
+        #within the given column, gather and reduce all output buffers
+        #storing in output buffer of specified row
+        for i in range(0,self.size):
+            if(i!=row):
+                self.PEGrid[row,col].outBuf=np.add(self.PEGrid[row,col].outBuf,self.PEGrid[i,col].outBuf)
+
+    def AllForwardConv(self,k,c):
+        #all PEs in the grid perform forward convolution of input buffer with layer weights
+        #must specify kernel index
+        for i in range(0,self.size):
+            for j in range(0,self.size):
+                #todo for now this is just one channel per row
+                self.PEGrid[i,j].ForwardConv(k,c)
+                
+    def ColAccum(self,row):
+        #accumulate elementwise what is in the output buffer of each PE within each column
+        #store accumulated result in the buffer of the specified row
+        for col in range(0,self.size):
+            for i in range(0,self.size):
+                if(i != row):
+                    print ("-------------------")
+                    print ("row is "+ str(row) + " and col is " + str(col))                            
+                    print (self.PEGrid[row,col].outBuf)
+                    print (self.PEGrid[i,col].outBuf)                    
+                    self.PEGrid[row,col].outBuf=np.add(self.PEGrid[row,col].outBuf,self.PEGrid[i,col].outBuf)
+                    print (self.PEGrid[row,col].outBuf)
+        
+    def Forward(self,layer):
+        #this function assumes that the input activations are buffered in each PE
+        #also assumes weights are buffered for given layer
+        #input activations are broadcast to input buffers of PEs within rows        
+        #then each PE performs local convolutions
+        #output buffers are accumulated within columns and stored as layer output for each row in a K loop
+
+        #TODO K loop here, start fresh for a new output row
+        for k in range(0,4):
+            #flush input and output buffers
+            for i in range(0,4):
+                for j in range(0,4):
+                    self.PEGrid[i,j].FlushInBuf()
+                    self.PEGrid[i,j].FlushOutBuf()
+
+            #loop through each column
+            for col in range(0,self.size):
+                #broadcast current column and print buffers
+                self.RowBroadcast(0,col,0)
+                self.RowBroadcast(1,col,0)
+                self.RowBroadcast(2,col,0)
+                self.RowBroadcast(3,col,0)
+                
+
+                #Perform convolution on first filter for all PEs in first row
+                #keep accumulating in the outbut buffer for all channels
+                self.AllForwardConv(k,col)
+
+            #once each column has broadcast, reduce in columns
+            #save accumulated outpout
+            
+            for c in range(0,self.size):
+                self.ColReduce(k,c)#TODO K loop is row
+                self.PEGrid[k,c].outAct[0]=self.PEGrid[k,c].outBuf
+                        
+
+
+
---
+++ arch/Controller.py
@@ -0,0 +1,78 @@
+import ArchLib
+import numpy as np
+print("Running Controller \n")
+
+C = ArchLib.Core(4)
+#C.PrintPEGrid()
+
+np.random.seed(0)
+
+#input activation dict
+InAct=dict()
+OutAct=dict()
+LayerWeights=dict()
+#_----------------------------
+#NOTE all arrays are in (C,H,W) and (K,C,H,W)
+#-----------------------------
+#create a 7x7x16 sample activation
+act = np.random.randint(5,size=(16,3,3))
+InAct[0]=act
+#create a 16,16,3,3  sample weight
+LayerWeights[0]=np.random.randint(5,size=(16,16,2,2))
+#---------------------------------------
+#basic test of convolution functionality
+testact=np.random.randint(5,size=(2,3,3))
+#print(testact)
+#testweights=np.random.randint(5,size=(2,2,2,2))
+#print(testweights)
+#testout=ArchLib.Convolve(testact,testweights)
+#print(testout)
+#---------------------------------------
+
+#------------------------------------------------------------------
+#as a baseline, perform forward pass convolutions
+#print out all activations for debug
+#print ("Original Activations")
+#print(InAct[0])
+#print out weights for debug
+#print ("Original weights")
+#print (LayerWeights[0])
+
+baseline=ArchLib.Convolve(InAct[0],LayerWeights[0])
+#print ("baseline --------")
+#print (baseline)
+#-------------------------------------------------------------------
+
+#split that activaiton in the C dimension and load into PEs
+#first row gets C=0,1,2,3 etc
+for i in range(0,4):
+    for j in range(0,4):
+        C.PEGrid[i,j].SetInAct(0,InAct[0][(i*4)+j,:,:])
+        
+#split the weights in K and C dimension and load into PEs
+for i in range(0,4):
+    for j in range(0,4):
+        #TODO K blocked loop insetad of hard coded
+        C.PEGrid[i,j].weights[0]=LayerWeights[0][j,(4*i):(4*(i+1)),:,:]
+        C.PEGrid[i,j].weights[1]=LayerWeights[0][j+4,(4*i):(4*(i+1)),:,:]
+        C.PEGrid[i,j].weights[2]=LayerWeights[0][j+8,(4*i):(4*(i+1)),:,:]
+        C.PEGrid[i,j].weights[3]=LayerWeights[0][j+12,(4*i):(4*(i+1)),:,:]
+
+#-----------------FORWARD PASS START---------------------------------
+#Each PE convolves its input activations with its local weights
+C.Forward(0)
+#Check forward convolution output against golden
+error=0
+for i in range(0,4):
+    for j in range(0,4):
+        if np.array_equal(baseline[(4*i)+j,:,:],C.PEGrid[i,j].outAct[0])!=1:
+            print ("UH OH ISSUE WITH OUTPUT INDEX {K}".format(K=(4*i)+j))
+            print(baseline[0,:,:])
+            print(C.PEGrid[i,j].outAct[0])
+            error=1
+if error==0:
+    print("forward pass convoultion output matches golden!!")
+#-----------------FORWARD PASS END---------------------------------
+
+
+#-----------------BACKWARD PASS ---------------------------------
